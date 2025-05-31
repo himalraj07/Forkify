10755a41aec007b18c175888490859644bfea00c
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-31 19:48:27 +0545

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a1a3b0c0-3eb2-4200-ba1e-f6d8d22cab6d';
+export const MODAL_CLOSE_SEC = 2.5;
